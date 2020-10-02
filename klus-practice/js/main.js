18531dfc17aebe84d88364f4585558a8e7b1e30f
[Students] Toms Krists Rumsons <92DPTKRumsons@rvt.lv>
2020-10-02 16:30:09 +0300

    ...

---
+++ klus-practice/js/main.js
@@ -0,0 +1,26 @@
+console.log("Welcome to KLUS!")
+
+function filter(btnType){
+    let btns = document.getElementsByClassName('filter-button')
+
+    for(let btn of btns){
+        btn.classList.remove('selected')
+        if(btn.id === btnType){
+            btn.classList.add('selected')
+        }
+    }
+
+    let table = document.getElementsByTagName('tr')
+    
+    for(let row of){
+        if(row.classname === ''){
+            continue
+        }
+
+        delite row.removeAttribute('hidden')
+
+        if(row.classname !== btnType && btnType !== 'viss'){
+            row.setAttribute('hidden','hidden')
+        }
+    }
+}
